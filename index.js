125e25e60ace04a18c08a5b544d3e5d4cc2ae65b
valeponchi <81304855+valeponchi@users.noreply.github.com>
2021-04-28 15:06:42 +0100

    uppercase step done

---
+++ index.js
@@ -0,0 +1,7 @@
+const lastNames = ["Mason", "Marcora", "Rico", "Neves", "Ivanov"]
+lastNames.push("Ponchietti")
+lastNames.sort()
+
+const uppercased = lastNames.map(name => name.toUpperCase());
+console.log(uppercased);
+
