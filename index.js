91c3dd315bbc457ae7825c8dd83a69afe9f36e9f
valeponchi <81304855+valeponchi@users.noreply.github.com>
2021-04-29 07:44:45 +0100

    no

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 const lastNames = ["Mason", "Marcora", "Rico", "Neves", "Ivanov"]
-lastNames.push("Ponchietti")
-lastNames.sort()
+let myLastName = prompt("What is your last name?")
 
-const uppercased = lastNames.map(name => name.toUpperCase());
-console.log(uppercased);
+lastNames.push(myLastName) //myLastName is added at the end of lastNames
+console.log("Here is the list with your last name in it = ", lastNames)
 
+lastNames.sort() // lastNames change posizions = I,Mac,MAs,NEv,Ponc,Rico
+console.log("Here is the list in alphabetical order = ", lastNames)
+
+console.log("Your last name position is = ", lastNames.indexOf(myLastName))
+
+const uppercased = lastNames.map(name => name.toUpperCase()); //uppercase exists with uppercased names
+console.log("All the last names are Uppercase-last-names: ", uppercased);
+
+
+//cannot find a way to do the challenge
+
+// const newArray = lastNames.map(name => [name, name.toUpperCase])
+// console.log("This is the New List = ", newArray)
+
+// const newArray = []
+// for (const lastName of lastNames) {
+//   newArray.push([lastName, lastName.toUpperCase])
+// }
+// console.log("This is the New List = ", newArray)
